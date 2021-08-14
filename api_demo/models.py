eb4a34117877e2461eb7f64c1e2825951e9e8c87
waro163 <waro163@163.com>
2021-08-14 21:10:25 +0800

    add Book model to api_demo app

---
+++ api_demo/models.py
@@ -0,0 +1,9 @@
+from exts import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    authors = db.Column(db.String(32))
+    publish_date = db.Column(db.Date())
+    price = db.Column(db.Float)
+    on_sale = db.Column(db.Boolean, default=True)
